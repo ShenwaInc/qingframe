73fc4c5cfdf950accee3e7de3d91db70a15a69a8
390557442 <www.1991un@qq.com>
2024-12-16 15:39:08 +0800

    更新优化多语言功能及默认首页展示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,35 +10,39 @@
         <link rel="stylesheet" href="{{ assets('/static/css/nunito.css') }}?v={{ QingRelease }}" />
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ assets('/static/css/welcome.css') }}?v={{ QingRelease }}" />
+        <link rel="stylesheet" href="{{ assets('/static/css/welcome.css') }}?v={{ TIMESTAMP }}" />
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="page-top"></div>
+        <div class="flex-bottom position-ref full-height">
+            <div class="page-top">
+                <span class="center text-black">@lang('轻如云')</span>
+            </div>
 
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="/console">@lang('控制台')</a>
-                        <a href="{{ url('auth/logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">@lang('logout')</a>
-                        <form id="logout-form" action="{{ url('auth/logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    @else
-                        <a href="{{ route('login') }}">@lang('控制台')</a>
-                    @endauth
-                    @if($Multilingual)
-                        @if($locale=='en')
-                            <a href="/?lang=zh">简体中文</a>
+            <nav class="top-nav">
+                @if (Route::has('login'))
+                    <div class="links">
+                        @auth
+                            <a href="/console">@lang('控制台')</a>
+                            <a href="{{ url('auth/logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">@lang('logout')</a>
+                            <form id="logout-form" action="{{ url('auth/logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         @else
-                            <a href="/?lang=en">English</a>
+                            <a href="{{ route('login') }}">@lang('控制台')</a>
+                        @endauth
+                        @if($Multilingual)
+                            @if($locale=='en')
+                                <a href="/?lang=zh">简体中文</a>
+                            @else
+                                <a href="/?lang=en">English</a>
+                            @endif
                         @endif
-                    @endif
-                </div>
-            @endif
+                    </div>
+                @endif
+            </nav>
 
             <div class="content">
-                <div class="title m-b-md">
+                <div class="title m-b-md text-black">
                     @lang("开放连接万事万物")
                 </div>
 
