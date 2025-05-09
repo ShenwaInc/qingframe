88678f07c09f8f3581d5a1078c546d7ddb9c0918
390557442 <www.1991un@qq.com>
2025-05-09 18:58:51 +0800

    更新AI-JS-SDK引用

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ assets('/static/css/welcome.css') }}?v={{ QingRelease }}" />
+        <script type="text/javascript" src="//ai.qingruyun.com/addons/swa_quickai/static/quick-ai.min.js?v=1.9"></script>
     </head>
     <body>
         <div class="flex-bottom position-ref full-height">
@@ -54,7 +55,7 @@
                     <a href="https://www.gxit.org/forum.php?mod=forumdisplay&fid=44" target="_blank">@lang('产品论坛')</a>
                     <a href="/console/module" target="_blank">@lang('应用市场')</a>
                     <a href="/console/report" target="_blank">@lang('提交工单')</a>
-{{--                    <a href="javascript:QuickAI.open(2, {acid: 1})"><img src="{{ asset('static/images/ai.png') }}" height="18" alt="QuickAi" /></a>--}}
+                    <a href="javascript:QuickAI.open(2, {acid: 1})"><img src="{{ asset('static/images/ai.png') }}" height="18" alt="QuickAi" /></a>
                 </div>
             </div>
         </div>
