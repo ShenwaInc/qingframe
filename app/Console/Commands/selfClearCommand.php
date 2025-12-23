c1190209ed9a952375336ef6f290d9c62b50541f
390557442@qq.com <390557442@qq.com>
2025-12-23 23:01:57 +0800

    优化自清理指令和数据库迁移脚本

--- app/Console/Commands/selfClearCommand.php
+++ app/Console/Commands/selfClearCommand.php
@@ -85,6 +85,7 @@ class selfClearCommand extends Command
             base_path('database/migrations/2021_08_10_113449_create_mc_groups_table.php'),
             base_path('database/migrations/2021_08_10_113449_create_mc_members_table.php'),
             base_path('database/migrations/2021_08_10_113449_create_core_paylog_table.php'),
+            base_path('database/migrations/2024_12_19_000000_add_balance_after_to_mc_credits_record_table.php')
         );
         foreach ($unused as $file){
             if (file_exists($file)){
